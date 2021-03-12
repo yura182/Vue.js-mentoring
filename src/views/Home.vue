487c0d9016ce4c114a2076f59d5d361a8304f83e
Yurii Petrashenko <yuriy.petrashenko7@gmail.com>
2021-03-12 12:10:49 +0200

    Generate new vue project
    - add Router
    - add Vuex
    - add Jest
    - add Storybook

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <FirstComponent msg="Vue-mentoring" />
+  </div>
+</template>
+
+<script>
+import FirstComponent from "@/components/FirstComponent.vue";
+
+export default {
+  name: "Home",
+  components: {
+    FirstComponent
+  }
+};
+</script>
